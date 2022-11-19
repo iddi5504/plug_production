02d5e20bd7f8ea7f4f933b28fc60f5cc8523b3aa
Iddi <Iddi5504@gmail.com>
2022-11-19 14:26:25 +0000

    lazy loading not working as expected. Add comment added

--- src/store/modules/makePostStore.js
+++ src/store/modules/makePostStore.js
@@ -34,7 +34,7 @@ const makePostStore = {
             context.commit('closeMakePost');
         },
         async makeRecommendation(context, data) {
-            // context.commit('showLoadScreen','Making recommendation',{root:true})
+            context.commit('showLoadScreen','Making recommendation',{root:true})
             const recommendData = data[0]
             const imageFile = data[1].imageFile
             const imageName = data[1].imageName
@@ -63,19 +63,19 @@ const makePostStore = {
                     const recommendationId= `${category}Recommendation${recommendationDoc.id}`
                     const completeRecommendationData = { ...recommendData, ...extraInfo, ...userData, id:recommendationId }
                     await setDoc(recommendationDoc, completeRecommendationData)
-                    // context.commit('closeMakePost')
-                    // context.commit('stopLoading',null,{root:true})
-                    // context.commit('alert', ['You have successfully made a recommendation.', 'Continue recommending to others, spread the word'], { root: true })
+                    context.commit('closeMakePost')
+                    context.commit('stopLoading',null,{root:true})
+                    context.commit('alert', ['You have successfully made a recommendation.', 'Continue recommending to others, spread the word'], { root: true })
 
                 })
                 .catch(()=>{
-                    // context.commit('stopLoading',null,{root:true})
+                    context.commit('stopLoading',null,{root:true})
 
                 })
 
         },
         async post(context, postData) {
-            // context.commit('showLoadScreen','Making your post',{root:true})
+            context.commit('showLoadScreen','Making your post',{root:true})
             const postUnrefinedData = postData[0]
             const postFile = postData[1].postFile
             const postFileName = postData[1].postFileName
@@ -99,13 +99,13 @@ const makePostStore = {
                         post, 
                         completePostData
                         )
-                    // context.commit('closeMakePost')
-                    // context.commit('alert', ['You have successfully made a post.', 'Check your notifications for activities on your post'], { root: true })
-                    // context.commit('stopLoading',null,{root:true})
+                    context.commit('closeMakePost')
+                    context.commit('alert', ['You have successfully made a post.', 'Check your notifications for activities on your post'], { root: true })
+                    context.commit('stopLoading',null,{root:true})
                 
                 })
                 .catch(()=>{
-                    // context.commit('stopLoading',null,{root:true})
+                    context.commit('stopLoading',null,{root:true})
                     
                 })
 
